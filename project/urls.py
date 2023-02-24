1c9c1620c9969609a5d41b2561a1eb9309e112d6
JaimeWIllianCarneiro <jwcs2@aluno.ifal.edu.br>
2023-02-24 09:28:25 -0300

    eu amo igual

--- project/urls.py
+++ project/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home, cadastro, store
+from app.views import home
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cadastro/', cadastro),
-    path('store/', store), 
+    # path('cadastro/', cadastro),
+    path('', home), 
 ]
